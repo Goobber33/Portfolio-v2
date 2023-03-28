d648af9c7e049a528b4a95e8c52cb36f52354b58
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-28 09:30:55 -0700

    Added some custom styling to the page.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <section id="about">
-      <div className="container">
+      <div className="container text-white">
         <h2>About Me</h2>
         <p>I am a software developer with a passion for building web applications. I enjoy solving complex problems and collaborating with others to build innovative solutions. In my free time, I enjoy hiking, playing guitar, and exploring new technologies.</p>
       </div>
