2202a5c2f90000691d8796588e78cc3305262f36
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-27 19:50:18 -0700

    Added an about me section. Need to reorganize it on the page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './index.css';
-import './App.css';
 import NavbarComponent from './components/NavbarComponent';
 import Portfolio from './components/Portfolio';
+import About from './components/About';
 
 const App: React.FC = () => {
   return (
     <>
       <NavbarComponent />
+      <About />
       <Portfolio />
     </>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <section id="about">
+      <div className="container">
+        <h2>About Me</h2>
+        <p>I am a software developer with a passion for building web applications. I enjoy solving complex problems and collaborating with others to build innovative solutions. In my free time, I enjoy hiking, playing guitar, and exploring new technologies.</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
