b9c60f10bdb63ef9fbd8a3465c9d054346c0a8f8
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-28 19:58:48 -0700

    Moved some items on the page, and added more bootstrap styling.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Hero from './components/Hero';
 const App: React.FC = () => {
   return (
     <>
-      <div className="container-fluid"> {/* Change to container-fluid */}
+      <div className="container-fluid">
         <div className="row">
           <div className="col-12 col-md-4">
             <LogoComponent />
@@ -19,15 +19,19 @@ const App: React.FC = () => {
           </div>
         </div>
       </div>
-      <Hero />
-      <div className="container text-white">
-        <h2 className="intro-text">
-          Hello! My name is Kyle Parks. I am a Full Stack Developer, and I love to create and learn new things. I am
-          currently enrolled in the University of Washingtons Full Stack coding bootcamp where I am learning new skills
-          every day. Please take a look at my Portfolio, and follow my social media links if you would like to get in
-          contact!
-        </h2>
+      <div className="container-fluid">
+        <div className="row"> {/* Add a Row component */}
+          <div className="col-12 col-md-8 col-lg-6 mx-auto"> {/* Add a Col component with mx-auto */}
+            <h2 className="intro-text text-white">
+              Hello! My name is Kyle Parks. I am a Full Stack Developer, and I love to create and learn new things. I am
+              currently enrolled in the University of Washingtons Full Stack coding bootcamp where I am learning new skills
+              every day. Please take a look at my Portfolio, and follow my social media links if you would like to get in
+              contact!
+            </h2>
+          </div>
+        </div>
       </div>
+      <Hero />
       <About />
       <Portfolio />
     </>
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,41 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faHtml5, faCss3, faJs } from '@fortawesome/free-brands-svg-icons';
 import { useSpring, animated } from 'react-spring';
 import useTypewriter from 'react-typewriter-hook';
+import { Element, scroller } from 'react-scroll';
 
 const About: React.FC = () => {
-  const [showIcons, setShowIcons] = React.useState(false);
+  const [showIcons, setShowIcons] = useState(false);
+  const [showTypewriter, setShowTypewriter] = useState(false); // Add a new state for the typewriter effect
   const iconAnimation = useSpring({
     opacity: showIcons ? 1 : 0,
     transform: showIcons ? 'translateY(0)' : 'translateY(100px)',
     delay: 500,
   });
 
-  const typewriterText = useTypewriter('About Me');
-  
-  
+  const typewriterText = useTypewriter(showTypewriter ? 'About Me' : ''); // Trigger the typewriter effect based on the state
 
-  React.useEffect(() => {
-    setShowIcons(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      const aboutSection = document.getElementById('about');
+      if (aboutSection) {
+        const rect = aboutSection.getBoundingClientRect();
+        const isInViewport = rect.top <= window.innerHeight && rect.bottom >= 0;
+        setShowIcons(isInViewport);
+        setShowTypewriter(isInViewport); // Set the typewriter effect state based on the visibility of the section
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const scrollToAbout = () => {
+    scroller.scrollTo('about', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
   return (
-    <section id="about" className="py-5">
-      <div className="container text-white">
-        <h2>
-          {typewriterText}
-        </h2>
-        <p>I am a software developer with a passion for building web applications. I enjoy solving complex problems and collaborating with others to build innovative solutions. In my free time, I enjoy hiking, playing guitar, and exploring new technologies.</p>
-        <animated.div style={iconAnimation}>
-          <FontAwesomeIcon icon={faReact} size="3x" className="mx-3 my-3 fa-icon" />
-          <FontAwesomeIcon icon={faHtml5} size="3x" className="mx-3 my-3 fa-icon" />
-          <FontAwesomeIcon icon={faCss3} size="3x" className="mx-3 my-3 fa-icon" />
-          <FontAwesomeIcon icon={faJs} size="3x" className="mx-3 my-3 fa-icon" />
-        </animated.div>
-      </div>
-    </section>
+    <Element name="about">
+      <section id="about" className="py-5">
+        <div className="container text-white d-flex flex-column align-items-center">
+          <h2 onClick={scrollToAbout}>
+            {typewriterText}
+          </h2>
+          <p>
+            I am a software developer with a passion for building web applications. I enjoy solving complex problems and
+            collaborating with others to build innovative solutions. In my free time, I enjoy hiking, playing guitar, and
+            exploring new technologies.
+          </p>
+          <animated.div style={iconAnimation}>
+            <FontAwesomeIcon icon={faReact} size="3x" className="mx-3 my-3 fa-icon" />
+            <FontAwesomeIcon icon={faHtml5} size="3x" className="mx-3 my-3 fa-icon" />
+            <FontAwesomeIcon icon={faCss3} size="3x" className="mx-3 my-3 fa-icon" />
+            <FontAwesomeIcon icon={faJs} size="3x" className="mx-3 my-3 fa-icon" />
+          </animated.div>
+        </div>
+      </section>
+    </Element>
   );
-}
+};
 
 export default About;
