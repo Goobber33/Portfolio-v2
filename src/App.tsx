d44f8d4ae25041b52d281efd0a77542473cebde0
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-29 10:03:28 -0700

    Created a IntroText.tsx file for my intro text, and removed it from my App.tsx file to add modularity to my portfolio app.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import About from './components/About';
 import Hero from './components/Hero';
 import SocialBar from './components/SocialMediaBar';
 import GlobalStyle from './components/GlobalStyle';
+import IntroText from './components/IntroText';
 
 const App: React.FC = () => {
   return (
@@ -22,18 +23,7 @@ const App: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-md-8 col-lg-6 mx-auto">
-          <h2 className="intro-text text-white pt-custom mb-5">
-              Hello! My name is Kyle Parks. I am a Full Stack Developer, and I love to create and learn new things. I am
-              currently enrolled in the University of Washington's Full Stack coding bootcamp where I am learning new skills
-              every day. Please take a look at my Portfolio, and follow my social media links if you would like to get in
-              contact!
-            </h2>
-          </div>
-        </div>
-      </div>
+      <IntroText />
       <About />
       <Hero />
       <Portfolio />
---
+++ src/components/IntroText.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const IntroText: React.FC = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 col-md-8 col-lg-6 mx-auto">
+          <h2 className="intro-text text-white pt-custom mb-5">
+            Hello! My name is Kyle Parks. I am a Full Stack Developer, and I love to create and learn new things. I am
+            currently enrolled in the University of Washington's Full Stack coding bootcamp where I am learning new skills
+            every day. Please take a look at my Portfolio, and follow my social media links if you would like to get in
+            contact!
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntroText;
