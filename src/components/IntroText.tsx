cfe1843b7ae1b1f84ab4e8370f339a4a81d97bba
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-31 16:07:56 -0700

    Added comments to my introtext file.

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 
+// Custom hook to animate text
 const useTextAnimation = (showText: boolean, delay: number) => {
   return useSpring({
     opacity: showText ? 1 : 0,
@@ -9,35 +10,58 @@ const useTextAnimation = (showText: boolean, delay: number) => {
   });
 };
 
+// Component that displays the introductory text
 const IntroText: React.FC = () => {
   const [showText, setShowText] = useState(false);
 
+  // Sets the "showText" state to true when the component mounts
   useEffect(() => {
     setShowText(true);
   }, []);
 
+  // Animations for the different parts of the text
   const helloTextAnimation = useTextAnimation(showText, 500);
   const nameTextAnimation = useTextAnimation(showText, 700);
   const descriptionTextAnimation = useTextAnimation(showText, 900);
 
   return (
+    // Container for the text
     <div className="container-fluid">
       <div className="row">
         <div className="col-12 col-md-8 col-lg-6 offset-md-1 offset-lg-2">
           <div className="intro-text-wrapper">
-            <animated.h2 style={{...helloTextAnimation, fontFamily: 'Garamond, serif'}} className="hello-text mb-3 text-white">
+            {/* Animated "Hi my name is" text */}
+            <animated.h2
+              style={{
+                ...helloTextAnimation,
+                fontFamily: 'Garamond, serif', // Sets the font for this text
+              }}
+              className="hello-text mb-3 text-white"
+            >
               Hi{' '}
               <span role="img" aria-label="waving hand" className="waving-hand fs-2">
                 👋 {' '}
               </span> my name is
             </animated.h2>
-            <animated.h3 style={{...nameTextAnimation, fontFamily: 'Adobe Caslon Pro, serif', fontWeight: 'bold'}} className="name-text fs-1 mb-4">
+            {/* Animated name text */}
+            <animated.h3
+              style={{
+                ...nameTextAnimation,
+                fontFamily: 'Adobe Caslon Pro, serif', // Sets the font for this text
+                fontWeight: 'bold', // Sets the font weight for this text
+              }}
+              className="name-text fs-1 mb-4"
+            >
               <span className="name-inner">
                 <span className="name-text-inner">Kyle Parks.</span>
               </span>
               <span className="underline"></span>
             </animated.h3>
-            <animated.h2 style={descriptionTextAnimation} className="text-white">
+            {/* Animated description text */}
+            <animated.h2
+              style={descriptionTextAnimation}
+              className="text-white"
+            >
               I am a Full Stack Developer, and I love to create and learn new things.
             </animated.h2>
           </div>
