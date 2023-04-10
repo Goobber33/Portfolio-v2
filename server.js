3caef9d75a316378ad1c28a9cee076d7fe9b8135
Goobber33 <kcparks1234@gmail.com>
2023-04-10 16:28:52 -0700

    Changed ports

--- server.js
+++ server.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.post('/send-email', async (req, res) => {
